19a8c0cdf9bb62373c6175f4f1c9f6e4af4c8c9d
Cloud84-blip <64501479+Cloud84-blip@users.noreply.github.com>
2023-07-26 12:16:25 +0200

    Clean actions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to the strat world!"', () => {
+      expect(appController.getHello()).toBe('Welcome to the strat world!');
     });
   });
 
